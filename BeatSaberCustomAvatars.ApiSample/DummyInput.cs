eb5f3ce218aba4692b1f32cd690b20c95ad2d0ca
Nicolas Gnyra <nicolas@gnyra.com>
2020-05-23 19:39:04 -0400

    Initial commit

---
+++ BeatSaberCustomAvatars.ApiSample/DummyInput.cs
@@ -0,0 +1,26 @@
+﻿using CustomAvatar.Tracking;
+using UnityEngine;
+
+namespace BeatSaberCustomAvatars.ApiSample
+{
+    internal class DummyInput : AvatarInput
+    {
+        public override bool TryGetHeadPose(out Pose pose)
+        {
+            pose = new Pose(new Vector3(0, 1.665f, -1), Quaternion.Euler(0, 0, 0));
+            return true;
+        }
+
+        public override bool TryGetLeftHandPose(out Pose pose)
+        {
+            pose = new Pose(new Vector3(-0.3f, 0.6f, -1), Quaternion.Euler(0, 0, 0));
+            return true;
+        }
+
+        public override bool TryGetRightHandPose(out Pose pose)
+        {
+            pose = new Pose(new Vector3(0.3f, 0.6f, -1), Quaternion.Euler(0, 0, 0));
+            return true;
+        }
+    }
+}
